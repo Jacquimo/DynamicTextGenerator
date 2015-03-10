9d6163a75a02a581c092717aa582af35f3280315
Gray Houston <ghousto@purdue.edu>
2015-03-10 00:19:07 -0400

    Created submission folder

---
+++ submission/Prefix.java
@@ -0,0 +1,125 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * Prefix class that represents prefixes used in a Dynamic Text Generator. A prefix can have a fixed but arbitrary number of context words.
+ * @author Gray Houston <grayhouston@purdue.edu>
+ * 
+ * @version 2/16/15
+ *
+ */
+public class Prefix {
+	
+	/**
+	 * The array of prefix strings that denote the start of a sentence is an array of empty strings. This method must be called and the array must be initialized
+	 * every time the program is trained on a new file so that the sentenceStartPrefixes array will have the correct length (i.e. correct number of elements).
+	 * <P>
+	 * If this method is not called every time a new file is trained, the program may inexplicably fail (especially if the length of the prefix has been changed).
+	 */
+	public static void initializeSentenceStartArray() {
+		// TODO: add code here
+	}
+	
+	/**
+	 * Returns a String[] whose contents (empty strings) are used to retrieve the Start-of-Sentence Prefix object.
+	 * <P>
+	 * This Start-of-Sentence Prefix object is special in that is used to determine the first word in a dynamically generated sentence.
+	 * @return a copy of the startOfSentencePrefixes array (using the Arrays class copyOf method)
+	 */
+	public static String[] getStartOfSentencePrefixes() {
+		// TODO: add code here
+		return null;
+	}
+	
+	/**
+	 * Constructor that takes an array of prefix strings as an argument
+	 * @param prefixStrings - the array of prefix strings
+	 */
+	public Prefix(String[] prefixStrings) {
+		// TODO: add code here
+	}
+	
+	/**
+	 * Returns the number of suffixes that this Prefix object contains
+	 * @return the value of the numSuffixes private variable
+	 */
+	public int getNumSuffixes() {
+		// TODO: add code here
+		return -1;
+	}
+	
+	/**
+	 * Returns the number of prefixes that this Prefix object contains
+	 * @return the length of the prefixes String[]
+	 */
+	public int getNumPrefixes() {
+		// TODO: add code here
+		return -1;
+	}
+	
+	/**
+	 * Returns the prefix string at a specified index.
+	 * @param index
+	 * @return
+	 */
+	public String getPrefixString(int index) {
+		// TODO: add code here
+		return null;
+	}
+	
+	/**
+	 * Returns the suffix string at a specified index
+	 * @param index
+	 * @return
+	 */
+	public String getSuffixString(int index) {
+		// TODO: add code here
+		return null;
+	}
+	
+	/**
+	 * Adds a suffix string to the array of all possible suffixes that appear after this prefix. This method allows for multiple copies of the same string
+	 * to be added to the array.
+	 * @param str - the string suffix (the word that appears directly after this prefix)
+	 */
+	public void addSuffix(String str) {
+		// TODO: add code here
+	}
+	
+	/**
+	 * Selects a random suffix from the array of suffixes. This function is integral to the sentence generation stage. This function works by using Math.random() to select
+	 * a valid index for the array of suffixes. This method returns the string at that index.
+	 * <P>
+	 * Warining: If the suffix array is empty (i.e. suffixes.length == 0), this method will throw an Array Index Out-of-Bounds Exception. Therefore, before calling this method,
+	 * you may want to check the number of suffixes first.
+	 * @return - a random suffix string (from the suffixes String[])
+	 */
+	public String getRandomSuffix() {
+		// TODO: add code here
+		return null;
+	}
+	
+	/**
+	 * Determines equality among Prefix objects. Two Prefix objects are considered equal if they both have the exact same string prefixes in the same order.
+	 * @param obj - Object to determine equality against
+	 */
+	public boolean equals(Object obj) {
+		// TODO: add code here
+		return false;
+	}
+	
+	/**
+	 * The string form of a prefix object is its list of prefixes converted to a whitespace delimited string
+	 */
+	public String toString() {
+		// TODO: add code here
+		return null;
+	}
+}
+
+
+
+
+
---
+++ submission/TextGenerationEngine.java
@@ -0,0 +1,210 @@
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * CS 180 - Dynamic Generation Project
+ * 
+ * This class prompts the user for an action and can dynamically generate sentences if the program has already been trained.
+ * 
+ * @author (Your Name) <(YourEmail@purdue.edu)>
+ * 
+ * @lab (Your Lab Section)
+ * 
+ * @version (Today's Date)
+ *
+ */
+public class TextGenerationEngine {	
+	private static String[] terminators = { ".", "!", "?" };
+	private static String[] trainedTexts = new String[8];
+	private static int numTextsTrained = 0;
+	private static StringArrayMap map = null;
+	private static int numSentances = 1;
+	
+	/**
+	 * Adds a file to the list of files that the program has been trained on
+	 * @param filename - name of the file to add
+	 */
+	public static void addTrainedTexts(String filename) {
+		trainedTexts[numTextsTrained++] = filename;
+		if (numTextsTrained >= trainedTexts.length) {
+			String[] temp = new String[2 * trainedTexts.length];
+			for (int i = 0; i < trainedTexts.length; ++i)
+				temp[i] = trainedTexts[i];
+			trainedTexts = temp;
+		}
+	}
+	
+	/**
+	 * Determines if the program has been trained on a file
+	 * @param filename - the name of the file to check
+	 * @return
+	 */
+	public static boolean haveTrainedText(String filename) {
+		for (int i = 0; i < numTextsTrained; ++i)
+			if (filename.equals(trainedTexts[i]))
+				return true;
+		
+		return false;
+	}
+	
+	public static void promptUser() {
+		System.out.println("0 - Terminate Program");
+		System.out.println("1 - Generate Sentence");
+		System.out.println("2 - Train Program on File");
+		System.out.println("3 - Change Number of Words in Prefix");
+		System.out.println("4 - Number of Sentences to Generate");
+		System.out.print("Action: ");
+	}
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in); // This scanner is closed in the switch case where the program is selected to end
+		while (true) {
+			int decision = -1;
+			
+			// Prompt user input/action
+			do {
+				promptUser();
+				try {
+					decision = in.nextInt();
+					
+				} catch (Exception e) {
+					decision = -1;
+				}
+				in.nextLine();
+			} while(decision < 0);
+			
+			switch(decision){
+			case 0:
+				System.out.printf("\nProgram Ending\n");
+				in.close();
+				return;
+				
+			case 1:
+				if (numTextsTrained < 1) {
+					System.out.printf("Program has not been trained yet\n\n");
+					break;
+				}
+				
+				System.out.printf("\nDynamically Generated Text\n\n");
+				for (int i = 0; i < numSentances; ++i) {
+					String sentence = generateSentence(map);
+					System.out.printf("%s\n\n", sentence);
+				}
+				break;
+				
+			case 2:
+				String filename = null;
+				do {
+					System.out.print("Enter file name ('0' for menu): ");
+					filename = in.next();
+					in.nextLine();
+					
+					// Allow user to return to main menu
+					if (filename.equals("0"))
+						break;
+					
+					// Check if program has already been trained on this text
+					if (haveTrainedText(filename)) {
+						System.out.printf("Program has already been trained on this text\n\n");
+						filename = null;
+						continue;
+					}
+					
+					// Check that the file is valid
+					File check = new File(filename);
+					if (!check.isFile()) {
+						System.out.printf("Invalid file name\n\n");
+						filename = null;
+						continue;
+					}
+					
+					PrefixGenerator.trainPrefixMap(map, filename);
+				
+				} while (filename == null);
+				
+				addTrainedTexts(filename);
+				System.out.println();
+				break;
+				
+			case 3:
+				int length = -1;
+				do {
+					System.out.print("Number of Words in Prefix: ");
+					length = in.nextInt();
+					if (length <= 0)
+						System.out.println("Invalid input");
+					in.nextLine();
+				} while (length <= 0);
+				
+				map = retrain(length);
+				
+				System.out.println("All texts re-trained\n");
+				break;
+				
+			case 4:
+				int num = -1;
+				do {
+					System.out.print("Num. of Sentences: ");
+					num = in.nextInt();
+					if (num < 0)
+						System.out.println("Invalid input");
+				} while (num < 0);
+				numSentances = num;
+				System.out.println();
+				break;
+				
+			default:
+				System.out.printf("Invalid program action\n\n");
+			}
+		}
+	}
+	
+	/**
+	 * Determines if a specified character is punctuation
+	 * @param c - the character to check
+	 * @return
+	 */
+	public static boolean isPunctuation(char c) {
+		return c == '.' || c == ',' || c == '?' || c == '!' || c == ';' || c == ':' || c == '"' ||
+				c == '(' || c == ')' || c == '\'';
+	}
+	
+	/**
+	 * Determines if the specified string is at the end of a sentence. A word is defined to be at the end of a sentence
+	 * if a string contained within the "terminators" list occurs at the end of the word before any non-punctuation 
+	 * characters occur. The terminator string doesn't need to be at the very end of the string for it to signal the end
+	 * of the sentence.
+	 * <P>
+	 * Examples <P>
+	 * goodbye." - end of sentence <P>
+	 * good. - end of sentence <P>
+	 * 1.5 - NOT end of sentence
+	 * @param suffix - the word to check
+	 * @return - true if word occurs at the end of a sentence
+	 */
+	public static boolean shouldTerminate(String suffix) {
+		// TODO: determine if the given string is at the end of a sentence
+		
+		return false;
+	}
+	
+	/**
+	 * Using a StringArrayMap of String[]'s to Prefix objects, dynamically generate a sentence by selecting a random suffix 
+	 * of the current prefix. 
+	 * @return
+	 */
+	public static String generateSentence(StringArrayMap map) {
+		// TODO: Implement text generation algorithm described in lab specification
+		return "";
+	}
+	
+	/**
+	 * Make a new StringArrayMap and train it on all of the files stored in the trainedtexts array, but with
+	 * the specified number of words in a prefix
+	 * @param prefixLength
+	 */
+	public static StringArrayMap retrain(int prefixLength) {
+		// TODO: implement the method
+		return null;
+	}
+}
