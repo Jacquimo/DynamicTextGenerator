8fd5fe8b6a2c6cbaf5038c29259b2dfcb8a44cec
Amr Ebaid <aebaid@purdue.edu>
2015-03-11 19:42:30 -0400

    TextGenerationEngine documentation

--- TextGenerationEngine.java
+++ TextGenerationEngine.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 /**
  * CS 180 - Dynamic Generation Project
  * 
- * This class prompts the user for an action and can dynamically generate sentences if the program has already been trained.
+ * This class prompts the user for an action and can dynamically generate
+ * sentences if the program has already been trained.
  * 
  * @author (Your Name) <(YourEmail@purdue.edu)>
  * 
@@ -13,16 +14,18 @@ import java.util.Scanner;
  * @version (Today's Date)
  *
  */
-public class TextGenerationEngine {	
+public class TextGenerationEngine {
 	private static String[] terminators = { ".", "!", "?" };
 	private static String[] trainedTexts = new String[8];
 	private static int numTextsTrained = 0;
 	private static StringArrayMap map = null;
 	private static int numSentances = 1;
-	
+
 	/**
 	 * Adds a file to the list of files that the program has been trained on
-	 * @param filename - name of the file to add
+	 * 
+	 * @param filename
+	 *            Name of the file to add
 	 */
 	public static void addTrainedTexts(String filename) {
 		trainedTexts[numTextsTrained++] = filename;
@@ -33,20 +36,26 @@ public class TextGenerationEngine {
 			trainedTexts = temp;
 		}
 	}
-	
+
 	/**
 	 * Determines if the program has been trained on a file
-	 * @param filename - the name of the file to check
-	 * @return
+	 * 
+	 * @param filename
+	 *            The name of the file to check
+	 * @return <code>true</code> if the model has been trained on that file,
+	 *         <code>false </code>otherwise
 	 */
 	public static boolean haveTrainedText(String filename) {
 		for (int i = 0; i < numTextsTrained; ++i)
 			if (filename.equals(trainedTexts[i]))
 				return true;
-		
+
 		return false;
 	}
-	
+
+	/**
+	 * A method used to prompt the user for desired action
+	 */
 	public static void promptUser() {
 		System.out.println("0 - Terminate Program");
 		System.out.println("1 - Generate Sentence");
@@ -55,61 +64,66 @@ public class TextGenerationEngine {
 		System.out.println("4 - Number of Sentences to Generate");
 		System.out.print("Action: ");
 	}
-	
+
+	/**
+	 * The main method used to run the engine
+	 */
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in); // This scanner is closed in the switch case where the program is selected to end
+		// scanner opened here, closed later when the program is selected to end
+		Scanner in = new Scanner(System.in);
+
 		while (true) {
 			int decision = -1;
-			
+
 			// Prompt user input/action
 			do {
 				promptUser();
 				try {
 					decision = in.nextInt();
-					
 				} catch (Exception e) {
 					decision = -1;
 				}
 				in.nextLine();
-			} while(decision < 0);
-			
-			switch(decision){
+			} while (decision < 0);
+
+			switch (decision) {
 			case 0:
 				System.out.printf("\nProgram Ending\n");
 				in.close();
 				return;
-				
+
 			case 1:
 				if (numTextsTrained < 1) {
 					System.out.printf("Program has not been trained yet\n\n");
 					break;
 				}
-				
+
 				System.out.printf("\nDynamically Generated Text\n\n");
 				for (int i = 0; i < numSentances; ++i) {
 					String sentence = generateSentence(map);
 					System.out.printf("%s\n\n", sentence);
 				}
 				break;
-				
+
 			case 2:
 				String filename = null;
 				do {
 					System.out.print("Enter file name ('0' for menu): ");
 					filename = in.next();
 					in.nextLine();
-					
+
 					// Allow user to return to main menu
 					if (filename.equals("0"))
 						break;
-					
+
 					// Check if program has already been trained on this text
 					if (haveTrainedText(filename)) {
-						System.out.printf("Program has already been trained on this text\n\n");
+						System.out
+								.printf("Program has already been trained on this text\n\n");
 						filename = null;
 						continue;
 					}
-					
+
 					// Check that the file is valid
 					File check = new File(filename);
 					if (!check.isFile()) {
@@ -117,15 +131,15 @@ public class TextGenerationEngine {
 						filename = null;
 						continue;
 					}
-					
+
 					PrefixGenerator.trainPrefixMap(map, filename);
-				
+
 				} while (filename == null);
-				
+
 				addTrainedTexts(filename);
 				System.out.println();
 				break;
-				
+
 			case 3:
 				int length = -1;
 				do {
@@ -135,12 +149,12 @@ public class TextGenerationEngine {
 						System.out.println("Invalid input");
 					in.nextLine();
 				} while (length <= 0);
-				
+
 				map = retrain(length);
-				
+
 				System.out.println("All texts re-trained\n");
 				break;
-				
+
 			case 4:
 				int num = -1;
 				do {
@@ -152,55 +166,70 @@ public class TextGenerationEngine {
 				numSentances = num;
 				System.out.println();
 				break;
-				
+
 			default:
 				System.out.printf("Invalid program action\n\n");
 			}
 		}
 	}
-	
+
 	/**
-	 * Determines if a specified character is punctuation
-	 * @param c - the character to check
-	 * @return
+	 * Determines if a given character is punctuation
+	 * 
+	 * @param c
+	 *            The character to check
+	 * @return <code>true</code> if it is a punctuation character,
+	 *         <code>false</code> otherwise
 	 */
 	public static boolean isPunctuation(char c) {
-		return c == '.' || c == ',' || c == '?' || c == '!' || c == ';' || c == ':' || c == '"' ||
-				c == '(' || c == ')' || c == '\'';
+		return c == '.' || c == ',' || c == '?' || c == '!' || c == ';'
+				|| c == ':' || c == '"' || c == '(' || c == ')' || c == '\'';
 	}
-	
+
 	/**
-	 * Determines if the specified string is at the end of a sentence. A word is defined to be at the end of a sentence
-	 * if a string contained within the "terminators" list occurs at the end of the word before any non-punctuation 
-	 * characters occur. The terminator string doesn't need to be at the very end of the string for it to signal the end
-	 * of the sentence.
+	 * Determines if the specified string is at the end of a sentence. A word is
+	 * defined to be at the end of a sentence if a string from the "terminators"
+	 * array occurs at the end of the word before any non-punctuation characters
+	 * occur. The terminator string doesn't need to be at the very end of the
+	 * string for it to signal the end of the sentence.
 	 * <P>
-	 * Examples <P>
-	 * goodbye." - end of sentence <P>
-	 * good. - end of sentence <P>
-	 * 1.5 - NOT end of sentence
-	 * @param suffix - the word to check
-	 * @return - true if word occurs at the end of a sentence
+	 * Examples:
+	 * <ul>
+	 * <li>"..... goodbye." - end of sentence</li>
+	 * <li>".....  good? ....." - end of sentence</li>
+	 * <li>"..... 1.5 ....." - NOT end of sentence</li>
+	 * </ul>
+	 * 
+	 * @param suffix
+	 *            The word to check
+	 * @return <code>true</code> if word occurs at the end of a sentence,
+	 *         <code>false</code> otherwise
 	 */
 	public static boolean shouldTerminate(String suffix) {
 		// TODO: remove exception and add code here
 		throw new UnsupportedOperationException();
 	}
-	
+
 	/**
-	 * Using a StringArrayMap of String[]'s to Prefix objects, dynamically generate a sentence by selecting a random suffix 
-	 * of the current prefix. 
-	 * @return
+	 * Using a StringArrayMap of String[]'s to Prefix objects, dynamically
+	 * generate a sentence by selecting a random suffix of the current prefix.
+	 * 
+	 * @return The generated sentence
 	 */
 	public static String generateSentence(StringArrayMap map) {
 		// TODO: remove exception and add code here
 		throw new UnsupportedOperationException();
 	}
-	
+
 	/**
-	 * Make a new StringArrayMap and train it on all of the files stored in the trainedtexts array, but with
-	 * the specified number of words in a prefix
+	 * Make a new {@link StringArrayMap} resulting from training the model on
+	 * all of the files stored in the {@link #trainedTexts} array, but with the
+	 * specified number of words in a prefix
+	 * 
 	 * @param prefixLength
+	 *            Number defining the count of words in a prefix
+	 * 
+	 * @return The new {@link StringArrayMap} resulting from retraining
 	 */
 	public static StringArrayMap retrain(int prefixLength) {
 		// TODO: implement the method
